f413efe14bf65a8569b8631fecd4a337c9885a72
Ridwan lawal <lawalridwan007@gmail.com>
2022-10-13 14:54:12 -0700

    add files for mandatory c codes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
